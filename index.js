4547cdbaa0edb62bed524727e892780c44abf58a
Hai Nguyen <hainguyen871112@gmail.com>
2018-12-09 17:30:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ function myAnimal() {
   return animal
 }
 
+var animal1 = 'cat'
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  var animal1 = 'cat'
   return animal1
 }
+yourAnimal()
 
 function add2(n) {
   const two = 2
@@ -23,9 +24,9 @@ var funkyFunction = function() {
     return "FUNKY!"
   }
 }
-
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 ()
+
 
